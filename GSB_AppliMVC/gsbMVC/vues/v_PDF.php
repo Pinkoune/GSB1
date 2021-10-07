4c1a99ea3d229f26427ec20a38b37deb60a61f50
Pinkoune <76524822+Pinkoune@users.noreply.github.com>
2021-10-07 16:03:43 +0200

    Affichage PDF v2

--- GSB_AppliMVC/gsbMVC/vues/v_PDF.php
+++ GSB_AppliMVC/gsbMVC/vues/v_PDF.php
@@ -11,63 +11,52 @@
     $PDF->AddPage();
     $PDF->SetFont("Arial","B",16);
     $PDF->SetTextColor(0,0,0);
-    $PDF->MultiCell(0, 10, "PDF de :\n" . $_SESSION['prenom'], 1, "C", 0);
-    $PDF->Image("../images/logo.jpg", 80, 40, 50, 50);
+    $PDF->MultiCell(0,10,utf8_decode("Fiche de frais hors forfait"),0,'C',0);
 
-    $position = 120; 
-    $requete2 = $bdd->query("SELECT * FROM Visiteur WHERE login = '$user';");
+    $position = 40;
+    $req = $bdd->query("SELECT id FROM Visiteur WHERE id = '$user';");
 
     $PDF->SetTextColor(0,0,0);
-
-    $PDF->SetY($position-16);
-    $PDF->SetX(75);
-    $PDF->MultiCell(60,8,utf8_decode("Prix Total"),1,'C');
-    $PDF->SetFont("Arial","",16);
-    $PDF->SetY($position-16);
-    $PDF->SetX(135);
 
     $PDF->SetFont("Arial","B",16);
     $PDF->SetY($position-8);
     $PDF->SetX(15);
-    $PDF->MultiCell(60,8,utf8_decode("Produit"),1,'C');
+    $PDF->MultiCell(60,8,utf8_decode("Date"),1,'C');
 
     $PDF->SetY($position-8);
     $PDF->SetX(75);
-    $PDF->MultiCell(60,8,utf8_decode("Prix"),1,'C');
+    $PDF->MultiCell(60,8,utf8_decode("Libellé"),1,'C');
 
     $PDF->SetY($position-8);
     $PDF->SetX(135);
-    $PDF->MultiCell(60,8,utf8_decode("Quantité"),1,'C');
+    $PDF->MultiCell(60,8,utf8_decode("Montant"),1,'C');
 
     $PDF->SetTextColor(0,0,0);
 
-    while ($donne = $requete2->fetch()) {
-      $idProduit = $donne['idProduit'];
-      $select = $bdd->query($bdd, "SELECT * FROM produit WHERE idProduit = '$idProduit';");
-      $donneesProduit = $select->fetch();
-      $PDF->SetFont("Arial","I",16);
+    //foreach ($lesFraisHorsForfait as $donneesVisiteur){
+    $mont = $req->fetch();
 
-      $PDF->SetY($position);
-      $PDF->SetX(15);
-      $PDF->MultiCell(60,8,utf8_decode($donneesProduit['titre']),1,'C');
+    $id = $mont['id'];
+    $select = $bdd->query("SELECT * FROM LigneFraisHorsForfait WHERE idVisiteur = '$id';");
+    $donneesVisiteur = $select->fetch();
+    $PDF->SetFont("Arial","I",16);
 
-      $PDF->SetY($position);
-      $PDF->SetX(75);
-      $PDF->MultiCell(60,8,utf8_decode($donneesProduit['prix']."e"),1,'C');
+    $PDF->SetY($position);
+    $PDF->SetX(15);
+    $PDF->MultiCell(60,8,($donneesVisiteur['date']),1,'C');
 
-      $PDF->SetY($position);
-      $PDF->SetX(135);
-      $PDF->MultiCell(60,8,utf8_decode($donne['quantite']),1,'C');
+    $PDF->SetY($position);
+    $PDF->SetX(75);
+    $PDF->MultiCell(60,8,($donneesVisiteur['libelle']),1,'C');
 
-      $position += 8;
-    }
+    $PDF->SetY($position);
+    $PDF->SetX(135);
+    $PDF->MultiCell(60,8,utf8_decode($donneesVisiteur['montant']." Euros"),1,'C');
+
+    $position += 8;
+    //}
 
     $PDF->Output();
-    $recupNbCommade = $bdd->query($bdd, "SELECT COUNT(*) AS nbCommade FROM commade WHERE userConnexion = '$user' ;");
-    $resultatNbCommade = $recupNbCommade->fetch();
-    $nbCommade = $resultatNbCommade['nbCommade'];
-    $nbCommade = $nbCommade + 1;
-    $PDF->Output("commande/".$user.$nbCommade.".PDF", "F");
   
   }
   else {
